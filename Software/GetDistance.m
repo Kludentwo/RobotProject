96118cdbd18a376e756df74a2b7ed14cf1de2127
RuneHeick <hteml@hotmail.com>
2015-10-01 10:15:30 +0200

    Software added

---
+++ Software/GetDistance.m
@@ -0,0 +1,7 @@
+function [ point_cloud ] = GetDistance( serial )
+%GETDISTANCE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ Software/Move.m
@@ -0,0 +1,7 @@
+function [ ] = Move( serial, motor1, motor2, time )
+%MOVE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
