3f15bf5cde1e1beab63179f8e96b48ccf56e8a39
Nicolai Glud <Kludentwo@gmail.com>
2015-10-02 15:20:11 +0200

    Robot Move and Turn is good

--- Software/Move.m
+++ Software/Move.m
@@ -1,6 +1,14 @@
 function [ ] = Move( socket, motor1, motor2, time )
 %MOVE Summary of this function goes here
 %   Detailed explanation goes here
-    fwrite(socket, uint8(['M','0',bitsra(motor1,8), (motor1), bitsra(motor2,8), (motor2),bitsra(time,8), (time),10]));
+    motor1msb = (bin2dec(num2str(bitget((int16(motor1)),16:-1:9))));
+    motor1lsb = (bin2dec(num2str(bitget((int16(motor1)),8:-1:1))));
+    motor2msb = (bin2dec(num2str(bitget((int16(motor2)),16:-1:9))));
+    motor2lsb = (bin2dec(num2str(bitget((int16(motor2)),8:-1:1))));
+    timemsb = (bin2dec(num2str(bitget((uint16(time)),16:-1:9))));
+    timelsb = (bin2dec(num2str(bitget((uint16(time)),8:-1:1))));
+       
+    %Henning = uint8(['M','0',bitsra(motor1,8), (motor1), bitsra(motor2,8), (motor2),bitsra(time,8), (time),10]);
+    fwrite(socket, uint8(['M','O',motor1msb, motor1lsb,motor2msb,motor2lsb,timemsb,timelsb,10]));
 end
 
--- Software/RobotController.m
+++ Software/RobotController.m
@@ -28,6 +28,23 @@ classdef RobotController
             Move(obj.socket,motor1, motor2, time);
         end; 
         
+        % Precondition: -180 <= phi <= 180;
+        % Postcondition: yada ydada
+        function [] = Turn(obj, phi)
+            if ( phi < 0 )
+                phi = abs(phi);
+                motor1 = 100;
+                motor2 = -100;
+                t = -2.127E-7*phi^4 + 2.596E-4*phi^3 - 0.067*phi^2 + 12.163*phi + 60.442;
+            else
+                motor1 = -100;
+                motor2 = 100;
+                t = -1.291E-7*phi^4 + 1.166E-4*phi^3 - 0.034*phi^2 + 9.391*phi + 67.849; % Time as a function of angle
+            end
+            
+            Move(obj.socket,motor1,motor2,t)
+        end;
+        
         function delete(obj)
             fclose(obj.socket);
         end
