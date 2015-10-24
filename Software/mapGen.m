7591aaef5c9ec65126f5b4d4170d26fc8be4d04b
rasorensen90 <rasorensen@hotmail.com>
2015-10-24 10:42:10 +0200

    New small map

--- Software/mapGen.m
+++ Software/mapGen.m
@@ -22,13 +22,13 @@ map(1:end,end)=1;
 map(1,1:end)=1;
 map(end,1:end)=1;
 map(1:45,end-66:end-59) = 1;
-map(38:45,end-66:end-29) = 1;
+map(38:45,end-81:end-44) = 1;
 save('smallmap','map')
 
 % Cost map
-costwidth = 28;
+costwidth = 35;
 se = strel('square', costwidth);
 costmap = imdilate(map,se);
-costmap = (costmap+map)*50+1;
+costmap = (costmap+map*25)*2+1;
 figure(1), imshow(costmap/max(max(costmap))), title('Dilated')
 save('smallcostmap','costmap')
